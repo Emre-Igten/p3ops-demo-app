c88b76329907905573c778ee4d63485ce835e65e
Emre Igten <emre.igten@student.hogent.be>
2023-11-05 16:45:47 +0100

    adad

--- src/Server/Program.cs
+++ src/Server/Program.cs
@@ -15,7 +15,7 @@ namespace Server
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
-                    webBuilder.UseUrls("http://0.0.0.0:5000");
+                    // webBuilder.UseUrls("http://0.0.0.0:5000");
                 });
     }
 }
